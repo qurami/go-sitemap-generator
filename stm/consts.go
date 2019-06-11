ce67e198480bd4b05be79a2d7e33c71a84fa55ab
9dan <32853831+9dan@users.noreply.github.com>
2019-06-11 17:49:13 +0200

    removed space

--- stm/consts.go
+++ stm/consts.go
@@ -9,10 +9,7 @@ const (
 var (
 	// IndexXMLHeader exists for create sitemap xml as a specific sitemap document.
 	IndexXMLHeader = []byte(`<?xml version="1.0" encoding="UTF-8"?>
-      <sitemapindex
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    >`)
+      <sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`)
 	// IndexXMLFooter and IndexXMLHeader will used from user together .
 	IndexXMLFooter = []byte("</sitemapindex>")
 )
@@ -20,9 +17,7 @@ var (
 var (
 	// XMLHeader exists for create sitemap xml as a specific sitemap document.
 	XMLHeader = []byte(`<?xml version="1.0" encoding="UTF-8"?>
-      <urlset
-         xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml"
-    >`)
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">`)
 	// XMLFooter and XMLHeader will used from user together .
 	XMLFooter = []byte("</urlset>")
 )
